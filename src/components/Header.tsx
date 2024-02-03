de7c6f6adfde87fba9ea5f1e7f0b24b63e080170
Michał <michalbystryk14@wp.pl>
2024-02-03 16:22:01 +0100

    feat: specify props; add LanguagePicker component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,31 @@
 import Image from 'next/image'
-import Logo from '../../public/logo.png'
+import Logo from '../../public/logo.svg'
+import LanguagePicker from './LanguagePicker'
 
-export default function Header() {
+type HeaderProps = {
+  currentLanguage: string
+  handleLanguageChange: (e: React.MouseEvent<HTMLLIElement>) => void
+}
+
+export default function Header({
+  currentLanguage,
+  handleLanguageChange
+}: HeaderProps) {
   return (
-    <header className="flex justify-center w-full bg-black/30 py-4 border-b border-white">
-      <Image
-        src={Logo}
-        alt="The Sadovsky Said Done Est. 2019"
-        width={80}
-        height={80}
-        quality={100}
-      ></Image>
+    <header className="w-full flex justify-center bg-black/25 p-4 border-b border-white">
+      <div className="relative flex justify-center w-full max-w-[1440px]">
+        <Image
+          src={Logo}
+          alt="The Sadovsky Said Done Est. 2019"
+          width={80}
+          height={80}
+          quality={100}
+        />
+        <LanguagePicker
+          currentLanguage={currentLanguage}
+          handleLanguageChange={handleLanguageChange}
+        />
+      </div>
     </header>
   )
 }
