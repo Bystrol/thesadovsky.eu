83998b7368a256d46c1a50b2a55af1b7f70aa7e2
Michał <michalbystryk14@wp.pl>
2024-02-03 19:21:03 +0100

    feat: add social media links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import Logo from '../../public/logo.svg'
 import LanguagePicker from './LanguagePicker'
+import { FaInstagram } from 'react-icons/fa'
+import { TbWorldWww } from 'react-icons/tb'
 
 type HeaderProps = {
   currentLanguage: string
@@ -13,7 +16,21 @@ export default function Header({
 }: HeaderProps) {
   return (
     <header className="absolute top-0 left-0 w-full flex justify-center bg-black/25 p-4 border-b border-white">
-      <div className="relative flex justify-center w-full max-w-[1440px]">
+      <div className="relative flex justify-center items-center w-full max-w-[1440px]">
+        <Link
+          href="https://www.instagram.com/thesadovsky_warszawa/"
+          target="_blank"
+          className="absolute left-0 hover:cursor-pointer"
+        >
+          <FaInstagram size={38} color="#fff" />
+        </Link>
+        <Link
+          href="https://thesadovsky.pl/"
+          target="_blank"
+          className="absolute left-[50px] hover:cursor-pointer"
+        >
+          <TbWorldWww size={38} color="#fff" />
+        </Link>
         <Image
           src={Logo}
           alt="The Sadovsky Said Done Est. 2019"
