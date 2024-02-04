606bcadfa5f09bedb728b2d1cba279867ddc4741
Michał <michalbystryk14@wp.pl>
2024-02-04 20:07:16 +0100

    feat: add LoadingScreen component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,38 @@
 'use client'
 
 import Header from '@/components/Header'
+import LoadingScreen from '@/components/LoadingScreen'
 import PromotionsSection from '@/components/PromotionsSection'
 import useLanguagePicker from '@/hooks/useLanguagePicker'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true)
   const { currentLanguage, handleLanguageChange, translation } =
     useLanguagePicker()
 
+  useEffect(() => {
+    setIsLoading(false)
+  }, [])
+
   return (
     <>
-      <Header
-        currentLanguage={currentLanguage}
-        handleLanguageChange={handleLanguageChange}
-      />
-      <main className="min-h-svh py-[105px]">
-        <PromotionsSection translation={translation} />
-      </main>
-      <footer className="w-full py-4">
-        <p className="text-center">© The Sadovsky Barbershop 2024</p>
-      </footer>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          <Header
+            currentLanguage={currentLanguage}
+            handleLanguageChange={handleLanguageChange}
+          />
+          <main className="min-h-svh py-[105px]">
+            <PromotionsSection translation={translation} />
+          </main>
+          <footer className="w-full py-4">
+            <p className="text-center">© The Sadovsky Barbershop 2024</p>
+          </footer>
+        </>
+      )}
     </>
   )
 }
